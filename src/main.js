e0b4f9a759295938d61147149c0a1d82beb0aaf5
jiao <751809930@qq.com>
2018-12-19 13:41:33 +0800

    路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter)
+import HelloWorld from './components/HelloWorld.vue'
+
+const Foo = { template: '<div>foo</div>' };
+const router = new VueRouter({
+  routes :[
+    {
+      path:'/index',
+      component:HelloWorld
+    },
+    {
+      path:'/foo',
+      component:Foo
+    }
+  ]
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
