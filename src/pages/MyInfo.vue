f755a8f5eba4ed72e60839bbd28119bd7cfbd42d
jiao <751809930@qq.com>
2018-12-26 19:41:06 +0800

    个人中心

---
+++ src/pages/MyInfo.vue
@@ -0,0 +1,36 @@
+<style scoped lang="less">
+  .cell-head-img{
+    width: 1.2em;
+    vertical-align: text-bottom;
+    margin-right: 6px;
+  }
+</style>
+
+<template>
+  <Group>
+    <Cell title="机构名称" value="少儿健康机构" >
+      <img slot="icon" src="../assets/icon/book.svg" class="cell-head-img" alt="">
+    </Cell>
+    <Cell title="角色" value="老师|顾问|教练" >
+      <img slot="icon" src="../assets/icon/book.svg" class="cell-head-img" alt="">
+    </Cell>
+    <Cell title="账号" value="13333333333" >
+      <img slot="icon" src="../assets/icon/book.svg" class="cell-head-img" alt="">
+    </Cell>
+    <Cell title="修改密码" value="********" is-link >
+      <img slot="icon" src="../assets/icon/book.svg" class="cell-head-img" alt="">
+    </Cell>
+  </Group>
+</template>
+
+<script>
+import { Group } from 'vux'
+import { Cell } from 'vux'
+export default {
+    
+  components: {
+      Group,
+      Cell
+  }
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import VueRouter from 'vue-router'
 import Login from './pages/Login.vue'
 import Main from './pages/Main.vue'
 import Data from './pages/Data.vue'
+import MyInfo from './pages/MyInfo.vue'
 
 export default new VueRouter({
 	routes: [
@@ -12,6 +13,10 @@ export default new VueRouter({
 				{
 					path: '/data',
 					component: Data
+				},
+				{
+					path: '/myinfo',
+					component: MyInfo
 				}
 			]
 		}
